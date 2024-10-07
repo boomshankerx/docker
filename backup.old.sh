92322c4c6c8883ac777cf930769beb6d1a9f6df1
Lorne Guse <boomshankerx@hotmail.com>
2024-10-07 17:47:37 -0600

    New backup script and updatelist.txt

--- backup.old.sh
+++ backup.old.sh
@@ -3,6 +3,7 @@
 BASE=/docker
 DEST=/backup
 DATE=$(date +%F-%H%M%S)
+
 echo $DATE
 
 mountpoint -q /backup
@@ -10,12 +11,11 @@ if (( $? )) ; then
     echo "/backup isn't mounted"
     mount -t nfs nas.techg:/mnt/tank/data/backup/_docker /backup
 fi
-
 cd $BASE
-for dir in */; do
-    dir=${dir%/}
-    echo $dir
-    zip -rv $DEST/$dir-$DATE.zip $dir
-done
+zip -rv $DEST/bind9-$DATE.zip bind9/config bind9/records bind9/cache
+zip -rv $DEST/headscale-$DATE.zip headscale/config headscale/data
+zip -rv $DEST/unifi-$DATE.zip unifi/config unifi/db
+zip -rv $DEST/portainer-$DATE.zip portainer/data
+zip -rv $DEST/duplicati-$DATE.zip duplicati
 
 find /backup -mtime +30 -print -exec rm {} \;
