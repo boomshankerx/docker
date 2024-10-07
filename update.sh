8567973806fc94e8a5a3be6cc16ad89134d18afa
Lorne Guse <boomshankerx@hotmail.com>
2024-10-07 10:50:49 -0600

    Update script changes

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-
-for dir in */; do
+while read -r dir; do
     echo $dir
     cd $dir
     docker compose pull
     docker compose up -d
     cd -
-done
+done < updatelist.txt
+docker image prune
 
-
+#for dir in */; do
+    #echo $dir
+    #cd $dir
+    #docker compose pull
+    #docker compose up -d
+    #cd -
+#done
